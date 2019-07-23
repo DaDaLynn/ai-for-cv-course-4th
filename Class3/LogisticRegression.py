e35b660db631560f8365194d8e554155faf97514
DaDaLynn <yunlinlong@163.com>
2019-07-23 23:21:29 +0800

    Update LogisticRegression.py

--- Class3/LogisticRegression.py
+++ Class3/LogisticRegression.py
@@ -1,59 +1,88 @@
 # complete logistic regression
-import math
 import random
-def inference(theta, x):
-    y = 1 / (1 + math.exp(- theta * x)))
+import numpy as np
+import matplotlib.pyplot as plt
+def sigmod(z):
+    y = 1 / (1 + np.exp(z))
     return y
+    
+def inference(w, b, x):
+    z = np.dot(w, x) + b
+    return sigmod(z)
 
-def eval_loss(theta, x_list, y_label):
-    _sum_loss = list(map(lambda x, y : y * math.log(inference(theta, x))
-                     + (1 - y) * math.log(1 - inference(theta, x)),
+def eval_loss(w, b, x_list, y_label):
+    _sum_loss = list(map(lambda x, y : y * np.log(inference(w, b, x))
+                     + (1 - y) * np.log(1 - inference(w, b, x)),
                      x_list, y_label))
     loss = sum(_sum_loss) / len(x_list)
     return loss
 
 def gradient(pred_y, gt_y, x):
     diff = gt_y - pred_y
-    diff_theta = diff * x
+    dw = diff * x
+    db = diff
+    return dw, db
 
-def calc_step_greadient(batch_x_list, gt_y_list, theta, lr):
+def calc_step_greadient(batch_x_list, gt_y_list, w, b, lr):
     batch_size = len(batch_x_list)
-    d_theta = 0
+    sum_dw = np.zeros(w.shape)
+    sum_db = 0
 
     for i in range(batch_size):
-        pred_y = inference(theta, batch_x_list[i])
-        d_theta += gradient(pred_y, gt_y_list[i], batch_x_list[i])
+        pred_y = inference(w, b, batch_x_list[i])
+        dw,db = gradient(pred_y, gt_y_list[i], batch_x_list[i])
+        sum_dw += dw
+        sum_db += db
     
-    d_theta /= batch_size
-    theta += lr * d_theta
-    return theta
+    dw = sum_dw / batch_size
+    db = sum_db / batch_size
+    w += lr * dw
+    b += lr * db
+    return w,b
 
-def train(sample_set, label_set, batch_size, lr, max_iter):
-    theta = 0
+def train(sample_set, label_set, dim, batch_size, lr, max_iter):
+    w = np.zeros(dim)
+    b = 0
+    nlen = sample_set.shape[0]
     for i in range(max_iter):
-        batch_ind = random.choice(len(sample_set), batch_size)
-        x_batch = [sample_set(j) for j in batch_ind]
+        batch_ind = np.random.choice(nlen, batch_size)
+        x_batch = [sample_set[j, :] for j in batch_ind]
         gt_y = [label_set[j] for j in batch_ind]
-        theta = calc_step_greadient(x_batch, gt_y, theta, lr)
+        w,b = calc_step_greadient(x_batch, gt_y, w, b, lr)
         if i % 100 == 0:
-            print("theta{0}".format(theta))
-            printf("loss is {0}".format(eval_loss(theta, sample_set, label_set)))
+            print("w = {0} b = {1}".format(w, b))
+            print("loss is {0}".format(eval_loss(w, b, sample_set, label_set)))
+    return w,b
             
-def gen_sample_data():
-    w = random.randint(0, 10) + random.random()		# for noise random.random[0, 1)
-    b = random.randint(0, 5) + random.random()
-    num_samples = 100
-    x_list = []
-    y_list = []
-    for i in range(num_samples):
-        x = random.randint(0, 100) * random.random()
-        y = w * x + b + random.random() * random.randint(-1, 1)
-        x_list.append(x)
-        y_list.append(y)
-    plt.figure(1)
-    plt.scatter(x_list, y_list)
-    plt.show()
-    return x_list, y_list, w, b
+def gen_bmi_data(len):
+    body_data = np.zeros([len, 3])
+    positive = []
+    negtive = []
+    for i in range(len):
+        body_data[i][0] = random.uniform(1.5, 2.4)
+        body_data[i][1] = random.uniform(40, 120)
+        if body_data[i][1] / (body_data[i][0]**2) < 22:
+            body_data[i][2] = 1 
+            positive.append([body_data[i][0], body_data[i][1]])
+        else:
+            negtive.append([body_data[i][0], body_data[i][1]])
+
+    #plt.figure(1)
+    #plt.scatter([i[0] for i in positive], [i[1] for i in positive], marker = "*")
+    #plt.scatter([i[0] for i in negtive], [i[1] for i in negtive], marker = "x")
+    #plt.show()
+    return body_data
+
+if __name__ == "__main__":
+    raw_data = gen_bmi_data(500)
+    dim = 2
+    sample_set = raw_data[:250, 0:2]
+    label_set = raw_data[:250, 2]
+    batch_size = 50
+    lr = 0.001
+    max_iter = 10000
+    w,b = train(sample_set[:250], label_set[:250], dim, batch_size, lr, max_iter)
+
 
 
 
